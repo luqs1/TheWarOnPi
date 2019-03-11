e9e8bd11c69d2c1f3170277208219d3b98002d4e
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-11 09:42:36 +0000

    Made it a function

--- Classes/Ultrasound.py
+++ Classes/Ultrasound.py
@@ -8,12 +8,11 @@ GPIO.setmode(GPIO.BCM)
 #set GPIO Pins
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
- 
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
+
+def distance(GPIO_TRIGGER,GPIO_ECHO):
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
  
