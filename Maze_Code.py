547a5f091c6eb921a6506f98b9f7814cd1fb5649
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-22 18:47:18 +0000

    Wiggle, Wiggle, Wiggle!

--- Maze_Code.py
+++ Maze_Code.py
@@ -22,37 +22,34 @@ IO.setup(f_Echo, IO.IN)
 timetoturn90 = 2 # WILL NEED TO BE CHANGED ON DAY IF USED
 a = 0
 x=0
-
+polling=0
 l = round(distance(Trigger,l_Echo),2)
 r = round(distance(Trigger,r_Echo),2)
 f = round(distance(Trigger,f_Echo),2)
 while True:
   x+=1
-  if x%5==0:
-      l = round(distance(Trigger, l_Echo), 2)
-      r = round(distance(Trigger, r_Echo), 2)
-      f = round(distance(Trigger, f_Echo), 2)
-  if x==30:
-      x=0
-      print(f,l,r, a)
-  if f > fThreshold :
-    if a!=1:
-      m.forward(speed,0,0)
-      a = 1
-  elif r < sThreshold and l > sThreshold:
-    if a!=2:
-      m.turn('l',0,speed)
-      a=2
-  elif r > sThreshold and l < sThreshold:
-    if a!=3:
-      m.turn('r',0,speed)
-      a=3
-  elif r > l:
-    if a!=3:
-      m.turn('r', 0, speed)
-      a=3
-  else:
-    if a!=2:
-      m.turn('l',0,speed)
-      a=2
+  if x % polling==0:
+    if x==5:
+        x=0
+        print(f,l,r,a)
+    if f > fThreshold :
+      if a!=1:
+        m.forward(speed,0,0)
+        a = 1
+    elif r < sThreshold and l > sThreshold:
+      if a!=2:
+        m.turn('l',0,speed)
+        a=2
+    elif r > sThreshold and l < sThreshold:
+      if a!=3:
+        m.turn('r',0,speed)
+        a=3
+    elif r > l:
+      if a!=3:
+        m.turn('r', 0, speed)
+        a=3
+    else:
+      if a!=2:
+        m.turn('l',0,speed)
+        a=2
 
