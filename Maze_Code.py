97e214f8c9818c159076a9c125abda44a2176755
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-22 17:56:49 +0000

    Wiggle, Wiggle, Wiggle!

--- Maze_Code.py
+++ Maze_Code.py
@@ -8,7 +8,7 @@ sThreshold = int(input('Side Threshold: '))
 fThreshold = int(input('Front Threshold: '))
 m = Movement()
 
-GPIO.setmode(GPIO.BCM)
+IO.setmode(IO.BCM)
 
 Trigger = 17
 l_Echo = 2
@@ -20,15 +20,20 @@ IO.setup(l_Echo, IO.IN)
 IO.setup(r_Echo, IO.IN)
 IO.setup(f_Echo, IO.IN)
 
+timetoturn90 = 2 # WILL NEED TO BE CHANGED ON DAY
+
 while True:
   l = distance(Trigger,l_Echo)
   r = distance(Trigger,r_Echo)
   f = distance(Trigger,f_Echo)
-  if f < fThreshold and r > sThreshold and l < sThreshold:
-    m.turn('r',0,speed)
-  elif f < fThreshold and r < sThreshold and l > sThreshold:
-    m.turn('l',0,speed)
-  elif f < fThreshold and r > sThreshold and l > sThreshold:
-    m.turn('r',0,speed)
-  else:
+  if f > fThreshold:
     m.forward(speed,0,0)
+  elif r < sThreshold and l > sThreshold:
+    m.turn('l',0,speed)
+  elif r > sThreshold and l < sThreshold:
+    m.turn('r',0,speed)
+  elif r > l:
+    m.turn('r', 0, speed)
+  else:
+    m.turn('l',0,speed)
+
