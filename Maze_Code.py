274f0722c1c75cccef7902893e6d0f9f76cba411
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-22 19:09:57 +0000

    Wiggle, Wiggle, Wiggle!

--- Maze_Code.py
+++ Maze_Code.py
@@ -2,6 +2,7 @@ import time
 import RPi.GPIO as IO
 from Classes.Ultrasound import *
 from Classes.Movement import *
+import time as t
 
 speed = 100
 sThreshold = int(input('Side Threshold: '))
@@ -22,34 +23,31 @@ IO.setup(f_Echo, IO.IN)
 timetoturn90 = 2 # WILL NEED TO BE CHANGED ON DAY IF USED
 a = 0
 x=0
-polling=0
-l = round(distance(Trigger,l_Echo),2)
-r = round(distance(Trigger,r_Echo),2)
-f = round(distance(Trigger,f_Echo),2)
-while True:
-  x+=1
-  if x % polling==0:
-    if x==5:
-        x=0
-        print(f,l,r,a)
-    if f > fThreshold :
-      if a!=1:
-        m.forward(speed,0,0)
-        a = 1
-    elif r < sThreshold and l > sThreshold:
-      if a!=2:
-        m.turn('l',0,speed)
-        a=2
-    elif r > sThreshold and l < sThreshold:
-      if a!=3:
-        m.turn('r',0,speed)
-        a=3
-    elif r > l:
-      if a!=3:
-        m.turn('r', 0, speed)
-        a=3
-    else:
-      if a!=2:
-        m.turn('l',0,speed)
-        a=2
 
+while True:
+  trigger(Trigger)
+  l,r,f = getUltra(l_Echo),getUltra(r_Echo),getUltra(f_Echo)
+  x+=1
+  if x==30:
+      x=0
+      print(f,l,r, a)
+  if f > fThreshold :
+    if a!=1:
+      m.forward(speed,0,0)
+      a = 1
+  elif r < sThreshold and l > sThreshold:
+    if a!=2:
+      m.turn('l',0,speed)
+      a=2
+  elif r > sThreshold and l < sThreshold:
+    if a!=3:
+      m.turn('r',0,speed)
+      a=3
+  elif r > l:
+    if a!=3:
+      m.turn('r', 0, speed)
+      a=3
+  else:
+    if a!=2:
+      m.turn('l',0,speed)
+      a=2
