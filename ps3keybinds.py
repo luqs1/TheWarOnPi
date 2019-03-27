9c871d1b5db9e419ef055503a8d12a1a8cb49a02
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-27 12:09:29 +0000

    Left distance to itself but also created component functions

--- ps3keybinds.py
+++ ps3keybinds.py
@@ -25,9 +25,9 @@ def deadZones(conInput):
 def mapper(coord,scaler,dir1):
     l,r=100,100
     if coord>0:
-        r= 100 - (100/128)*abs(coord)
-    elif coord<0:
         l= 100 - (100/128)*abs(coord)
+    elif coord<0:
+        r= 100 - (100/128)*abs(coord)
     l *= scaler
     r *= scaler
     return int(abs(l)),int(abs(r)),dir1,dir1
@@ -54,18 +54,14 @@ for event in device.read_loop():
         if event.code == 5:
             R2 = event.value
             if R2 > 0:
-                if R2 < trigThresh:
-                    m.stop()
-                else:
+                if R2 > trigThresh:
                     out = mapper(x,R2/255,0)
                     m.set(*out)
+        """
         if event.code == 0:
             L2  = event.value - 128
-            if L2 >  0:
-                if L2 < trigThresh:
-                    m.stop()
-                else:
+            if L2 <  0:
                     out = mapper(x, L2 / 255, 1)
                     m.set(*out)
         print(L2,R2,x)
-
+        """
