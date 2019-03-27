1ac5ca39021ecf41c1bf0d5e4e9963999c345479
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-27 14:36:32 +0000

    Trying to access pixy from the Git

--- Classes/Vision.py
+++ Classes/Vision.py
@@ -1,6 +1,26 @@
-import os
+from pixy import *
+from ctypes import *
+import time as t
+
+number = 2
+class Blocks(Structure):
+    _fields_ = [("type", c_uint),
+                ("signature", c_uint),
+                ("x", c_uint),
+                ("y", c_uint),
+                ("width", c_uint),
+                ("height", c_uint),
+                ("angle", c_uint)]
+
 class Vision():
     def __init__(self):
-        pass
-    def get_colour(self,signature):
-        pass
+        pixy_init()
+        self.blocks = BlockArray(number)
+        frame = 0
+    def get_block(self):
+        blocks = self.blocks
+        count = pixy_get_blocks(number, self.blocks)
+        return [(blocks[index].type, blocks[index].signature,
+                 blocks[index].x, blocks[index].y, blocks[index].width,
+                 blocks[index].height) for index in range(0,count)
+                 ]
