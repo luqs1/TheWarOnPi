45ba40e84b87e441209d680c3111b5df46b4bc2e
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-27 01:09:05 +0000

    Left distance to itself but also created component functions

--- Classes/Ultrasound.py
+++ Classes/Ultrasound.py
@@ -26,6 +26,31 @@ def distance(GPIO_TRIGGER,GPIO_ECHO):
     # and divide by 2, because there and back
     s = (TimeElapsed * 34300) / 2
     return s
+def trigger(GPIO_TRIGGER):
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+def getU(GPIO_ECHO):
+    StartTime = time.time()
+    StopTime = time.time()
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    s = (TimeElapsed * 34300) / 2
+    return s
 if __name__ == '__main__':
  GPIO_TRIGGER = int(input('Trigger: '))
  GPIO_ECHO = int(input('Echo: '))
