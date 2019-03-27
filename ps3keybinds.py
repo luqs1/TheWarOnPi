b6a7af4129aba571fff6a82c6582281778f8a6fe
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-27 12:13:24 +0000

    Left distance to itself but also created component functions

--- ps3keybinds.py
+++ ps3keybinds.py
@@ -3,7 +3,7 @@ from Classes.Movement import *
 m = Movement()
 speed = int(input('Speed: ')) % 101
 deadzone = 10
-buttonMap = {'310':[m.turn,('l',0,speed)],'311':[m.turn,('r',0,speed)],'316':[exit,()]}
+buttonMap = {'310':[m.turn,('l',0,speed)],'311':[m.turn,('r',0,speed)],'316':[exit,()],'304':[m.stop,()]}
 trigThresh = int(input('Trigger Thresholds: '))
 for i in buttonMap.values():
     i.append(False)
