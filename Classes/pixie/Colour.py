c7e942154138ff9b27fd4ba688dba17e3e769809
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-28 19:44:51 +0000

    Added print avgs

---
+++ Classes/pixie/Colour.py
@@ -0,0 +1,25 @@
+from Movement import *
+from Vision2 import  *
+m = Movement()
+v = Vision()
+area_limit = 1000
+for i in range(2):
+    done_colour = False
+    while not done_colour:
+        blocks = v.get_colour(i)
+        if blocks == None:
+            m.turn('r')
+        else:
+            for block in blocks:
+                x = block.m_x
+                y = block.m_y
+                a = block.m_width * block.m_height
+            if a <= area_limit:
+                if abs(x-160) <= 30:
+                    m.forward(20)
+                elif x > 160:
+                    m.turn('r')
+                else:
+                    m.turn('l')
+            elif a > area_limit:
+                done_colour = True
