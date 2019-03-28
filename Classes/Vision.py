fc92dd3c8bf31a1043279980e5d4b2ec68091c90
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-28 13:57:45 +0000

    for pixy2

--- Classes/Vision.py
+++ Classes/Vision.py
@@ -1,35 +1,35 @@
-import sys
-sys.path.append('~/pixy/build/libpixyusb_swig')
-from pixy import *
+import pixy
 from ctypes import *
-import time as t
+from pixy import *
+
+# Pixy2 Python SWIG get blocks example #
+
+print ("Pixy2 Python SWIG Example -- Get Blocks")
+
+pixy.init ()
+pixy.change_prog ("color_connected_components");
+
+class Blocks (Structure):
+  _fields_ = [ ("m_signature", c_uint),
+    ("m_x", c_uint),
+    ("m_y", c_uint),
+    ("m_width", c_uint),
+    ("m_height", c_uint),
+    ("m_angle", c_uint),
+    ("m_index", c_uint),
+    ("m_age", c_uint) ]
 
 number = 2
-class Blocks(Structure):
-    _fields_ = [("type", c_uint),
-                ("signature", c_uint),
-                ("x", c_uint),
-                ("y", c_uint),
-                ("width", c_uint),
-                ("height", c_uint),
-                ("angle", c_uint)]
-
 class Vision():
     def __init__(self):
-        pixy_init()
         self.blocks = BlockArray(number)
-        frame = 0
+        self.frame = 0
     def get_block(self):
         blocks = self.blocks
-        count = pixy_get_blocks(number, self.blocks)
+        count = ccc_get_blocks(number, self.blocks)
         if count > 0:
-            return [{'type': blocks[index].type,
-                     'signature': blocks[index].signature,
-                     'x': blocks[index].x,
-                     'y':blocks[index].y,
-                     'width': blocks[index].width,
-                 'height':blocks[index].height
-                     } for index in range(0,count)]
+            self.frame += 1
+            return [blocks[index] for index in range(0,count)]
         else:
             return None
     def get_colour(self,signature):
@@ -39,5 +39,3 @@ class Vision():
             if line['signature'] == signature:
                 result.append(line)
         return result
-
-v = Vision()
