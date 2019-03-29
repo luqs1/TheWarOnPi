4c2c393fb8023711d327b7c4f094b95fcc07791d
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-29 21:35:26 +0000

    5

--- Amazing_Code.py
+++ Amazing_Code.py
@@ -50,10 +50,10 @@ while True:
  
     if averageFront > fThreshold:
         m.forward(speed, 0, 0)
-    elif averageLeft < sThreshold:
-        m.turn('r', 0.3, speed)
-    elif averageRight < sThreshold:
-        m.turn('l', 0.3, speed)
+        if averageLeft < sThreshold:
+            m.turn('r', 0.3, speed)
+        elif averageRight < sThreshold:
+            m.turn('l', 0.3, speed)
     elif averageRight > averageLeft:
         m.turn90('r')
     elif averageLeft > averageRight:
