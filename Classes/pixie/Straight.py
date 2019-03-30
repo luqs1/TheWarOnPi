a2ac09c42763e5adafe66290a80d6c5970f52870
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-30 13:04:19 +0000

    turn stuff

--- Classes/pixie/Straight.py
+++ Classes/pixie/Straight.py
@@ -12,7 +12,7 @@ while True:
     print(line)
     if line != None:
         angle = degrees(tan((line.m_x1 -line.m_x0)/(line.m_y1 - line.m_y0 + 0.00000000001)))
-        if abs(angle) < 15:
+        if abs(angle) < 10:
             m.forward()
         elif angle > 0:
             m.turn('r',0,tSpeed)
