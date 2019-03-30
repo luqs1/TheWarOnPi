02d0223ed1052a4d5aac4798a53d713b48eb355e
L.Ahmed <luqmaanahmed@hotmail.com>
2019-03-30 00:52:57 +0000

    Set values

--- Classes/pixie/Straight.py
+++ Classes/pixie/Straight.py
@@ -7,10 +7,11 @@ v.setmode(2)
 v.lamp(0,1)
 while True:
     line = v.get_lowest_line()
-    angle = degrees(tan((line.m_x1 -line.m_x0)/(line.m_y1 - line.m_y0)))
-    if abs(angle) < 10:
-        m.forward()
-    elif angle > 0:
-        m.turn('r')
-    else:
-        m.turn('l')
+    if line != None:
+        angle = degrees(tan((line.m_x1 -line.m_x0)/(line.m_y1 - line.m_y0)))
+        if abs(angle) < 10:
+            m.forward()
+        elif angle > 0:
+            m.turn('r')
+        else:
+            m.turn('l')
